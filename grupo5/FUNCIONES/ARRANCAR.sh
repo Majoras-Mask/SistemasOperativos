162f919c41db8a89775439c10c0a54a2cb2b4d60
Kevin <addinkevin@gmail.com>
2015-10-07 01:45:18 -0300

    Ya funca Arrancar y Detener.

--- grupo5/FUNCIONES/ARRANCAR.sh
+++ grupo5/FUNCIONES/ARRANCAR.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 if [ -z "$CONFDIR" ] || [ -z "$BINDIR" ] || [ -z "$MAEDIR" ] || [ -z "$NOVEDIR"] || [ -z "$ACEPDIR"] || [ -z "$PROCDIR"] || [ -z "$REPODIR"] || [ -z "$LOGDIR" ] || [ -z "$RECHDIR" ]; then
-	return 1
+	echo "Variables de entorno no inicializadas."
+	exit 1
 else
-	pid=$(ps aux | grep $1 | grep -v 'Arrancar' | grep -v 'grep' | head -n 1 | awk '{print $2}')
+	pid=$(ps aux | grep $1 | grep -v 'ARRANCAR' | grep -v 'grep' | head -n 1 | awk '{print $2}')
 	if [ -z $pid ]; then
 		./$1 &
-		return 0
+		echo $!
+		exit 0
 	else
-		return 1
+		echo "No es posible arrancar $1 ya que esta siendo ejecutado."
+		exit 1
 	fi
 fi
 
---
+++ grupo5/FUNCIONES/DETENER.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+pid=$(ps aux | grep $1 | grep -v 'DETENER' | grep -v 'grep' | head -n 1 | awk '{print $2}')
+if [ ! -z $pid ]; then
+	kill $pid
+	echo "Se detuvo el proceso $1 con PID: $pid"
+	exit 0
+else
+	echo "No se pudo detener el proceso $1, ya que no esta siendo ejecutado."
+	exit 1
+fi
