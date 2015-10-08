14e1c50be26a1dc28fd311134c6f93a8d766591e
Seba0703 <sebastian.a.rocha@hotmail.com>
2015-10-08 20:25:07 -0300

    arreglos

--- grupo5/AFRAINIC/AFRAINIC.sh
+++ grupo5/AFRAINIC/AFRAINIC.sh
@@ -2,35 +2,28 @@
 
 GRUPO="$PWD"
 MAEDIR="MAEDIR"
-BINDIR="BINDIR"
 # Variable CONFDIR, directorio de configuracion
 CONF=$(echo "$PWD" | sed -n 's-^\(.*\)grupo5.*-\1grupo5/conf/AFRAINST.conf-p')
+BINDIR="BINDIR"
 NOVEDIR="NOVEDIR"
 RECHDIR="RECHDIR"
 REPODIR="REPODIR"
 ACEPDIR="ACEPDIR"
 PROCDIR="PROCDIR"
 LOGDIR="LOGDIR"
-
-# Archivos a generar en este script
-LOGFILE="AFRAINIC.log"
+LOGEXT=$(grep '^LOGEXT' "$CONF" | cut -d '=' -f 2)
 RETORNO=""
 
 Loguear(){
-    echo "$1"
-    echo "$1" >> "$LOGDIR/$LOGFILE"
-}
-
-LeerInput(){
-    read input
-    echo "$input" >> "$LOGDIR/$LOGFILE"
+	echo "$1"
+	"$BINDIR"/GRALOG.sh "AFRAINIC" "$1" "$2"
 }
 
 LeerSiONo(){
     local mensaje=$1
     while true; do
-        Loguear "$mensaje"
-        LeerInput
+	Loguear "$mensaje" "INFO"
+        read input
         
         if [ $input = "Si" -o $input == "SI" -o $input == "si" -o $input == "Y"\
             -o $input == "y" -o $input == "Yes" ]; 
@@ -51,18 +44,17 @@ loguearVariables(){
 	local tipoDir=('Configuración' 'Ejecutables' 'Maestros y Tablas' 'Archivos de Log')
 	
 	for(( i=0;i<${#listar[@]};i++ )); do
-		Loguear "Directorio de ${tipoDir[${i}]}: ${listar[${i}]}"
-		Loguear "$(ls "${listar[${i}]}")"
+		Loguear "Directorio de ${tipoDir[${i}]}: ${listar[${i}]} $(ls "${listar[${i}]}") " "INFO"
 	done
 
 	local listar2=("$NOVEDIR" "$ACEPDIR" "$PROCDIR" "$REPODIR" "$RECHDIR")
 	local tipoDir2=('recepcion de archivos de llamamdas' 'Archivos de llamadas Aceptados' 'Archivos de llamadas Sospechosas' 'Archivos de Reportes de llamadas' 'Archivos Rechazados')
 	
 	for(( i=0;i<${#listar2[@]};i++ )); do
-		Loguear "Directorio de ${tipoDir2[${i}]}: ${listar2[${i}]}"
+		Loguear "Directorio de ${tipoDir2[${i}]}: ${listar2[${i}]}" "INFO"
 	done
 	
-	Loguear "Estado de Sistema: INICIALIZADO"
+	Loguear "Estado de Sistema: INICIALIZADO" "INFO"
 }
 
 verificaInstalacionCompleta(){
@@ -82,7 +74,7 @@ verificaInstalacionCompleta(){
 
 	BINDIR=$(grep '^BINDIR' "$CONF" | cut -d '=' -f 2)
 
-	local bin=('AFRAINIC.sh')
+	local bin=('AFRAINIC.sh' 'AFRARECI' 'AFRAUMBR.sh' 'ARRANCAR.sh' 'clasificarLLamada.sh' 'DETENER.sh' 'GRALOG.sh' 'MOVERA.sh' 'parserLLamada.sh' 'validarCampos.sh' 'validarLLamada.sh' 'verificarUmbrales.sh')
 	local cantBin=${#bin[@]}
 
 	for(( j=0; j<$cantBin; j++ )); do
@@ -93,8 +85,8 @@ verificaInstalacionCompleta(){
 	done
 	
 	if [ ${#faltantes[@]} -ne 0 ]; then
-		Loguear "Archivos faltantes en la instalación: ${faltantes[*]}"
-		Loguear "Ejecute instalador...................................."
+		Loguear "Archivos faltantes en la instalación: ${faltantes[*]}" "ERR"
+		Loguear "Ejecute instalador: ./AFRAINST.sh" "ERR"
 		return 1		
  	fi
 	
@@ -111,7 +103,7 @@ verificarPermisos(){
 		if ! [ -x "$BINDIR/$file" ]; then
 			chmod +x "$BINDIR/$file"
 			if [ "$?" = -1  ]; then
-      				Loguear "No se pudo setear los permisos de $BINDIR/$file"
+      				Loguear "No se pudo setear los permisos de $BINDIR/$file" "ERR"
 				return 1     
     			fi
 		fi			
@@ -121,7 +113,7 @@ verificarPermisos(){
 		if ! [ -r "$MAEDIR/$file" ]; then
 			chmod +r "$MAEDIR/$file"
 			if [ "$?" = -1  ]; then
-                                Loguear "No se pudo setear los permisos de $MAEDIR/$file"
+                                Loguear "No se pudo setear los permisos de $MAEDIR/$file" "ERR"
 				return 1
 			fi
 
@@ -140,10 +132,11 @@ inicializarAmbiente(){
 	export ACEPDIR=$(grep '^ACEPDIR' "$CONF" | cut -d '=' -f 2)
 	export RECHDIR=$(grep '^RECHDIR' "$CONF" | cut -d '=' -f 2)
 	export PROCDIR=$(grep '^PROCDIR' "$CONF" | cut -d '=' -f 2)
-	export REPDIR=$(grep '^REPODIR' "$CONF" | cut -d '=' -f 2)
+	export REPODIR=$(grep '^REPODIR' "$CONF" | cut -d '=' -f 2)
 	export NOVEDIR=$(grep '^NOVEDIR' "$CONF" | cut -d '=' -f 2)
 	export LOGDIR=$(grep '^LOGDIR' "$CONF" | cut -d '=' -f 2)
 	export LOGSIZE=$(grep '^LOGSIZE' "$CONF" | cut -d '=' -f 2)
+	export LOGEXT=$(grep '^LOGEXT' "$CONF" | cut -d '=' -f 2)
 	
 }
 	
@@ -152,30 +145,30 @@ inicializarAmbiente(){
 arrancarAFRAECI(){
 
 	LeerSiONo "¿Desea efectuar la activación de AFRARECI? Si – No"
-		
+	"$BINDIR"/GRALOG.sh "AFRAINIC" "$RETORNO" "INFO"
+	
 	if [ $RETORNO == "No" ]; then
-		Loguear "Debe ingresar el comando ./Arrancar AFRARECI.sh por consola."
+		Loguear "Debe ingresar el comando ./ARRANCAR.sh AFRARECI.sh por consola." "INFO"
 	else	
 		pid=$(ps aux | grep "AFRARECI" | grep -v 'ARRANCAR' | grep -v 'grep' | head -n 1 | awk '{print $2}')
 		
 		if [ ! -z "$pid" ]; then  
-			Loguear "Proceso AFRARECI ya iniciado. Debe utilizar el comando DETENER para terminarlo"
+			Loguear "Proceso AFRARECI ya iniciado. Debe utilizar el comando DETENER para terminarlo: ./DETENER.sh AFRARECI.sh" "WAR"
 		else
-			#"$BINDIR/AFRARECI" &
+			"$BINDIR"/AFRARECI &
 			ID=$!
-			Loguear "AFRARECI corriendo bajo el no.: $ID" 
+			Loguear "AFRARECI corriendo bajo el no.: $ID" "INFO"
 		fi
 	fi
 }
 
 Inicializar(){
-	echo "CONF:$CONF"
 	
     if [ -z "$GRUPO" ] || [  -z "$CONFDIR" ] || [ -z "$BINDIR" ] || [ -z "$MAEDIR" ] ||
 		[ -z "$NOVEDIR" ] || [ -z "$ACEPDIR" ] || [ -z "$PROCDIR" ] || [ -z "$REPODIR" ]
 		[ -z "$LOGDIR" ] || [ -z "$RECHDIR" ]; then
-		Loguear "Ambiente ya inicializado, para reiniciar termine la sesión e ingrese nuevamente"  
-        else
+		Loguear "Ambiente ya inicializado, para reiniciar termine la sesión e ingrese nuevamente" "INFO"
+        else	
 		verificaInstalacionCompleta
 		local completa=$?
 		
@@ -191,5 +184,4 @@ Inicializar(){
 		fi
 	fi	
 }
-
 Inicializar
