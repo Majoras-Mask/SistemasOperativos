d29202c4de0695543812ea14262dc427bd821788
mtejedor <mtejedor@hexacta.com>
2015-10-19 20:32:50 -0300

    fix en arrancar y detener cuando no recibe parametros

--- grupo5/FUNCIONES/ARRANCAR.sh
+++ grupo5/FUNCIONES/ARRANCAR.sh
@@ -3,14 +3,18 @@ if [ -z "$CONFDIR" ] || [ -z "$BINDIR" ] || [ -z "$MAEDIR" ] || [ -z "$NOVEDIR" 
 	echo "Variables de entorno no inicializadas."
 	exit 1
 else
-	pid=$(ps aux | grep "$1" | grep -v 'ARRANCAR' | grep -v 'grep' | grep '/bin/bash' | head -n 1 | awk '{print $2}')
-	if [ -z $pid ]; then
-		"$BINDIR/$1" &
-		echo $!
-		exit 0
-	else
-		echo "No es posible arrancar $1 ya que esta siendo ejecutado."
+	if [ -z $1 ]; then
+		echo "Parametro faltante"
 		exit 1
+	else
+		pid=$(ps aux | grep "$1" | grep -v 'ARRANCAR' | grep -v 'grep' | grep '/bin/bash' | head -n 1 | awk '{print $2}')
+		if [ -z $pid ]; then
+			"$BINDIR/$1" &
+			echo $!
+			exit 0
+		else
+			echo "No es posible arrancar $1 ya que esta siendo ejecutado."
+			exit 1
+		fi
 	fi
 fi
-
--- grupo5/FUNCIONES/DETENER.sh
+++ grupo5/FUNCIONES/DETENER.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
-pid=$(ps aux | grep "$1" | grep -v 'DETENER' | grep -v 'grep' | grep '/bin/bash' | head -n 1 | awk '{print $2}')
-if [ ! -z $pid ]; then
-	kill $pid
-	echo "Se detuvo el proceso $1 con PID: $pid"
-	exit 0
-else
-	echo "No se pudo detener el proceso $1, ya que no esta siendo ejecutado."
+if [ -z $1 ]; then
+	echo "Parametro faltante"
 	exit 1
+else
+	pid=$(ps aux | grep "$1" | grep -v 'DETENER' | grep -v 'grep' | grep '/bin/bash' | head -n 1 | awk '{print $2}')
+	if [ ! -z $pid ]; then
+		kill $pid
+		echo "Se detuvo el proceso $1 con PID: $pid"
+		exit 0
+	else
+		echo "No se pudo detener el proceso $1, ya que no esta siendo ejecutado."
+		exit 1
+	fi
 fi
